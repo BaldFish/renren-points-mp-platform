b35ee50f03a2ef76a12f0f6522a03d31ffe99707
lwc <wence.lv@cnlaunch.com>
2019-07-05 11:26:21 +0800

    好友邀请构建

---
+++ src/components/shareHint/shareHint.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="shareHint">
+    shareHint
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "shareHint",
+    components: {},
+    data() {
+      return {}
+    },
+    created() {
+    },
+    beforeMount() {
+    },
+    mounted() {
+    },
+    watch: {},
+    computed: {},
+    methods: {},
+  }
+</script>
+
+<style scoped lang="stylus">
+  .shareHint {
+  }
+</style>
---
+++ src/components/shareLogin/shareLogin.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="shareLogin">
+    shareLogin
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "shareLogin",
+    components: {},
+    data() {
+      return {}
+    },
+    created() {
+    },
+    beforeMount() {
+    },
+    mounted() {
+    },
+    watch: {},
+    computed: {},
+    methods: {},
+  }
+</script>
+
+<style scoped lang="stylus">
+  .shareLogin {
+  }
+</style>
