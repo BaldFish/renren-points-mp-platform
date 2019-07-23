e738d9c189a0aac7c312a14a29b4ef8543f24125
lwc <wence.lv@cnlaunch.com>
2019-07-23 10:30:44 +0800

    1

--- src/components/shareHint/shareHint.vue
+++ src/components/shareHint/shareHint.vue
@@ -15,7 +15,7 @@
         shareTitle:"人人积分商城",
         shareDesc:"邀新人注册赚积分，换好礼",
         shareUrl:location.origin+`/shareLogin?invitationCode=${this.userId}`,
-        shareImg:location.origin+"/static/images/share_logo.jpg",
+        shareImg:location.origin+"/static/images/share_logo.png",
         userId:"",
       }
     },
